3e8b4c71dfc7d1e3e951b97df07e62b5d47e6159
SwiftMage <evangjones@gmail.com>
2025-07-29 15:11:22 -0400

    Initial Commit

---
+++ CalcBox/CalcBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalcBoxApp.swift
+//  CalcBox
+//
+//  Created by Evan Jones on 7/29/25.
+//
+
+import SwiftUI
+
+@main
+struct CalcBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
